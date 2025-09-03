5e871d252d0c0aa7c9a0a4698dc9c2215b450401
Makepeace123 <makepeacebanelez@gmail.com>
2025-09-03 08:52:00 +0200

    Update 01_install_dependencies.py

--- 01_install_dependencies.py
+++ 01_install_dependencies.py
@@ -1,1 +1,7 @@
+# -*- coding: utf-8 -*-
+"""01_install_dependencies.py"""
 
+# Install required packages
+!pip install xgboost shap tensorflow scikit-learn plotly -q
+
+print("All packages installed successfully!")
