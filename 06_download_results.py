e8b7aedd0caa977e4ef35ba9817a41ff21b4ad8d
Makepeace123 <makepeacebanelez@gmail.com>
2025-09-03 08:55:47 +0200

    Update 06_download_results.py

--- 06_download_results.py
+++ 06_download_results.py
@@ -1,1 +1,12 @@
+# -*- coding: utf-8 -*-
+"""06_download_results.py"""
 
+from google.colab import files
+import os
+
+# Create ZIP archive
+!zip -r eswatini_forecast_results.zip model_artifacts/ data/
+
+# Download
+files.download('eswatini_forecast_results.zip')
+print("Results downloaded successfully!")
