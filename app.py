b4af62362b104878e92df86c39889c7331c868ba
Makepeace123 <makepeacebanelez@gmail.com>
2025-09-03 22:16:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import plotly.graph_objects as go
 # Page configuration
 # -----------------------------
 st.set_page_config(
-    page_title="Eswatini Agriculture Forecast",
+    page_title="Eswatini Agriculture Forecast HUB",
     page_icon="🌱",
     layout="wide",
     initial_sidebar_state="expanded"
